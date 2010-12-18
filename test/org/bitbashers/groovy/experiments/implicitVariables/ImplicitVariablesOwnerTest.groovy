346b75d6b496746fefd2c0e0e2a79bec857c61dc
Ed Sumerfield <esumerfd@bitbashers.org>
2010-12-18 18:56:34 -0500

    Add proof of how the implicit variables work

---
+++ test/org/bitbashers/groovy/experiments/implicitVariables/ImplicitVariablesOwnerTest.groovy
@@ -0,0 +1,41 @@
+package org.bitbashers.groovy.experiments.implicitVariables;
+
+class ImplicitVariablesOwnerTest extends GroovyTestCase {
+    
+    void test_owner_same_as_parent_this() {
+        
+        def testInstance = this
+        
+        def closure = {
+            assertEquals testInstance, this
+            assertEquals this, owner
+        }
+        
+        closure()
+    }
+    
+    void test_delegate_defaults_to_parent_this() {
+        
+        def testInstance = this
+        
+        def closure = {
+            assertEquals testInstance, this
+            assertEquals this, delegate
+        }
+        
+        closure()
+    }
+    
+    void test_delegate_reassigned_to_affect_closure_this() {
+        
+        def alternateContext = "some object"
+        
+        def closure = {
+            delegate = alternateContext
+            
+            assertEquals "SOME OBJECT", toUpperCase()
+        }
+        
+        closure()
+    }
+}
