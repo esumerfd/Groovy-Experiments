4a0808efe70ced29d227cbfc6f5c3d62ce3fa7db
Ed Sumerfield <esumerfd@bitbashers.org>
2011-02-24 11:48:35 -0500

    Correct delegate test

--- test/org/bitbashers/groovy/experiments/delegate/DelegateTest.groovy
+++ test/org/bitbashers/groovy/experiments/delegate/DelegateTest.groovy
@@ -1,41 +1,45 @@
 package org.bitbashers.groovy.experiments.delegate;
 
 class DelegateTest extends GroovyTestCase {
+  
+  void test_delegate() {
+    def foo = new FooProxy()
     
-    void test_delegate() {
-        def foo = new FooProxy()
-        
-        assertEquals "default instance", foo.info()
-    }
+    assertEquals "default instance", foo.info()
+  }
+  
+  void test_dynamic_delegation() {
+    def foo = new FooProxy()
+    foo.x = new Foo("dynamic foo instance")
     
-    void test_dynamic_delegation() {
-        def foo = new FooProxy()
-        foo.x = new Foo("dynamic foo instance")
-        
-        assertEquals "dynamic foo instance", foo.info()
-    }
+    assertEquals "dynamic foo instance", foo.info()
+  }
+  
+  void test_assigning_a_new_delegate_at_construction_time() {
+    def foo = new FooProxy( new Foo("assigned in constructor") )
     
-    void test_assigning_a_new_delegate_at_construction_time() {
-        def foo = new FooProxy( new Foo("assigned in constructor") )
-        
-        assertEquals "assigned in constructor", foo.info()
-    }
+    assertEquals "assigned in constructor", foo.info()
+  }
 }
 
 class Foo {
-    def text
-    Foo(text) {
-        this.text = text
-    }
-    def info() {
-        text
-    }
+  def text
+  Foo(text) {
+    this.text = text
+  }
+  def info() {
+    text
+  }
 }
 
 class FooProxy {
-    @Delegate Foo x = new Foo("default instance")
-    
-    FooProxy(newX) {
-        x = newX
-    }
+  @Delegate Foo x
+  
+  FooProxy() {
+    x = new Foo("default instance")
+  }
+  
+  FooProxy(newX) {
+    x = newX
+  }
 }
