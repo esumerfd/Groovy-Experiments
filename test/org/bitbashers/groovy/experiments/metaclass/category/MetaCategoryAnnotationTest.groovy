8b6d73529b80dd6a45eecfc9c9f06d68a3540456
Ed Sumerfield <esumerfd@bitbashers.org>
2011-03-13 11:20:07 -0400

    Add tests for runtime mixin and category annotation.

---
+++ test/org/bitbashers/groovy/experiments/metaclass/category/MetaCategoryAnnotationTest.groovy
@@ -0,0 +1,29 @@
+package org.bitbashers.groovy.experiments.metaclass.category
+
+class MetaCategoryAnnotationTest extends GroovyTestCase {
+  
+  void test_using_scoped_use() {
+    
+    use(BarMix) {    
+      assertEquals "mixed in", new Bar().mixedIn()
+    }
+  }
+  
+  void test_method_not_included_if_no_use() {
+    
+    assertEquals "original method", new Bar().mixedIn()
+  }
+}
+
+class Bar {
+  def mixedIn() {
+    "original method"
+  }
+}
+
+@Category(Bar)
+class BarMix {
+  def mixedIn() {
+    "mixed in"
+  }
+}
---
+++ test/org/bitbashers/groovy/experiments/metaclass/mixin/MetaRuntimeMixinTest.groovy
@@ -0,0 +1,30 @@
+package org.bitbashers.groovy.experiments.metaclass.mixin
+
+import groovy.util.GroovyTestCase;
+
+class MetaRuntimeMixinTest extends GroovyTestCase {
+  
+  void test_mixin() {
+    
+    Bar.mixin BarMix
+    
+    assertEquals "mixed in", new Bar().mixedIn()
+  }
+  
+  /*
+   * MUST RUN BOTH TESTS FOR THIS TO WORK
+   */
+  void test_mixin_persistent_accross_jvm() {
+    
+    assertEquals "mixed in", new Bar().mixedIn()
+  }
+}
+
+class Bar {
+}
+
+class BarMix {
+  def mixedIn() {
+    "mixed in"
+  }
+}
