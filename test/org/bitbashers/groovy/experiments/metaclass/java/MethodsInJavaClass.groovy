0e94f5f944f1dd6a1bbf74bdaed662c140c64fba
Ed Sumerfield <esumerfd@bitbashers.org>
2012-02-26 10:11:40 -0500

    prove we can add a metachange to a java class

--- test/org/bitbashers/groovy/experiments/metaclass/java/MethodsInJavaClass.groovy
+++ test/org/bitbashers/groovy/experiments/metaclass/java/MethodsInJavaClass.groovy
@@ -1,11 +1,10 @@
 package org.bitbashers.groovy.experiments.metaclass.java
 
 class MethodsInJavaClassTest extends GroovyTestCase {
-  
-  void test_add_groovy_method_to_java_class() {
-      Date.metaClass.formatGmtMillis = {
-      }
 
-      println new Date().formatGmtMillis()
+  void test_add_groovy_method_to_java_class() {
+    Date.metaClass.getFormatGmtMillis = { "formatted time" }
+
+    assertEquals "formatted time",  new Date().formatGmtMillis
   }
 }
